084b38e42753a0f1c78eee4eb43b114d30dd34e5
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2020-11-10 14:16:34 +0100

    handle locale in alphabetical order

--- src/alphabeticalOrder/alphabeticalOrder.ts
+++ src/alphabeticalOrder/alphabeticalOrder.ts
@@ -2,12 +2,11 @@ import { SortDirections, SortOptions } from "../sortByProperty/sortByProperty.in
 
 
 export const SortAlphabeticalOrder = (collection: any[], options: SortOptions): any[] => {
-
     return collection.sort((a: Record<string, string>, b: Record<string, string>) => {
         return options.direction === SortDirections.Ascending
             // sorting by ascending order
-            ? a[options.sortKey].localeCompare(b[options.sortKey])
+            ? a[options.sortKey].localeCompare(b[options.sortKey], options.locale)
             // sorting by descending order
-            : b[options.sortKey].localeCompare(a[options.sortKey])
+            : b[options.sortKey].localeCompare(a[options.sortKey], options.locale)
     });
 }
