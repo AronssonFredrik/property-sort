4669944866acac6e216de6b7e228557e9c8aae68
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2020-11-10 17:20:09 +0100

    Tiddy up test code

--- src/alphabeticalOrder/alphabeticalOrder.test.ts
+++ src/alphabeticalOrder/alphabeticalOrder.test.ts
@@ -8,50 +8,58 @@ let array = [
 ];
 
 describe('Sort by alphabetical order', () => {
-    test('In ascending order', () => {
-        array = SortAlphabeticalOrder(array, {
-            direction: SortDirections.Ascending,
-            sortKey: 'name'
+    describe('Handle ascending order', () => {
+        beforeEach(() => {
+            array = SortAlphabeticalOrder(array, {
+                direction: SortDirections.Ascending,
+                sortKey: 'name'
+            });
         });
+        test('Alpha should be first', () => expect(array[0].name).toBe('Alpha'));
+        test('Zeta should be last', () => expect(array[array.length - 1].name).toBe('Zeta'));
 
-        expect(array[0].name).toBe('Alpha');
     });
-    test('In descending order', () => {
-        array = SortAlphabeticalOrder(array, {
-            direction: SortDirections.Descending,
-            sortKey: 'name'
-        });
+    describe('Handle descending order', () => {
+        beforeEach(() => {
+            array = SortAlphabeticalOrder(array, {
+                direction: SortDirections.Descending,
+                sortKey: 'name'
+            });
+        })
 
-        expect(array[0].name).toBe('Zeta');
+        test('Zeta should be first', () => expect(array[0].name).toBe('Zeta'));
+        test('Alpha should be last', () => expect(array[array.length - 1].name).toBe('Alpha'));
     });
 
     describe('Handle diacritic characters', () => {
+        let indexOfZ: number, indexOfÄ: number;
         // add an object with name that includes diacritic character
         beforeEach(() => array = [...array, { name: 'Ärling' }]);
-        test('in German, ä sorts before z', () => {
-            array = SortAlphabeticalOrder(array, {
-                direction: SortDirections.Ascending,
-                sortKey: 'name',
-                locale: 'de'
+
+        describe('In German', () => {
+            beforeEach(() => {
+                array = SortAlphabeticalOrder(array, {
+                    direction: SortDirections.Ascending,
+                    sortKey: 'name',
+                    locale: 'de'
+                });
+                indexOfÄ = array.findIndex(item => item.name === 'Ärling');
+                indexOfZ = array.findIndex(item => item.name === 'Zeta');
             });
+            test('Ä sorts before z', () => expect(indexOfÄ).toBeLessThan(indexOfZ));
 
-            const indexOfÄ = array.findIndex(item => item.name === 'Ärling');
-            const indexOfZ = array.findIndex(item => item.name === 'Zeta');
-
-            expect(indexOfÄ).toBeLessThan(indexOfZ);
         });
-        test('in Swedish, ä sorts after z', () => {
-            array = SortAlphabeticalOrder(array, {
-                direction: SortDirections.Ascending,
-                sortKey: 'name',
-                locale: 'se'
+        describe('In Swedish', () => {
+            beforeEach(() => {
+                array = SortAlphabeticalOrder(array, {
+                    direction: SortDirections.Ascending,
+                    sortKey: 'name',
+                    locale: 'se'
+                });
+                indexOfÄ = array.findIndex(item => item.name === 'Ärling');
+                indexOfZ = array.findIndex(item => item.name === 'Zeta');
             });
-
-            const indexOfÄ = array.findIndex(item => item.name === 'Ärling');
-            const indexOfZ = array.findIndex(item => item.name === 'Zeta');
-
-
-            expect(indexOfÄ).toBeGreaterThan(indexOfZ);
+            test('Ä sorts after z', () => expect(indexOfÄ).toBeGreaterThan(indexOfZ));
         });
     });
 }); 
