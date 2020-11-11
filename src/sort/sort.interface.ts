b6b1af3aeeb3c8104cb8932a534239998eb45c80
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2020-11-11 21:13:39 +0100

    initiate jsdocs

--- src/sort/sort.interface.ts
+++ src/sort/sort.interface.ts
@@ -1,8 +1,24 @@
+/**
+ * @param {SortDirections} direction - uses type of SortDirections
+ * @param {string} sortKey - property key to sort by
+ * @param {string} locale - the locale to sort by, use ISO 639-1 Language Code
+ * @extends Intl.CollatorOptions
+ * @example { 
+ *  direction: SortDirections.Ascending,
+ *  sortKey: 'id',
+ *  locale: 'se',
+ *  numeric: true,
+ *  ignorePunctuation: true
+ * }
+ */
 export interface SortOptions extends Intl.CollatorOptions {
     direction: SortDirections;
     sortKey: string;
     locale?: string; // to create ISO enum interface
 }
+/**
+ * @enum {SortDirections} None = 0; Ascending = 1; Descending = 2
+ */
 export enum SortDirections {
     None,
     Ascending,
--- src/sort/sort.ts
+++ src/sort/sort.ts
@@ -1,7 +1,19 @@
 import { SortAlphabeticalOrder } from "./alphabetical/alphabetical";
 import { SortOptions } from "./sort.interface";
 
-// todo
+/**
+ * @module
+ * @function SortByProperty
+ * @param collection - array to sort
+ * @param {SortOptions} options - typeof SortOptions
+ * @example { 
+ *  direction: SortDirections.Ascending,
+ *  sortKey: 'id',
+ *  locale: 'se',
+ *  numeric: true
+ * }
+ * @returns sorted array
+ */
 export default (collection: any[], options: SortOptions): any[] => {
     const sortType = collection.length && typeof collection[0][options.sortKey];
     switch (sortType) {
