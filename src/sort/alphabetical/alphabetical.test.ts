f2cb3736db05cace15e85479df1319e46e9d21d7
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2020-11-14 22:52:32 +0100

    Updated alphabetical order functional to be more simplified

--- src/sort/alphabetical/alphabetical.test.ts
+++ src/sort/alphabetical/alphabetical.test.ts
@@ -1,111 +1,62 @@
 import { SortDirections } from "../sort.interface";
 import { SortAlphabeticalOrder } from "./alphabetical";
 
-interface alphabeticalCollection {
-    name: string;
-    catCount: string;
-}
+let returnVal: number;
 
-let collection: alphabeticalCollection[];
-const baseArray: alphabeticalCollection[] = [
-    {
-        name: 'Zeta',
-        catCount: '2 cats'
-    },
-    {
-        name: 'Beta',
-        catCount: '10 cats'
-    },
-    {
-        name: 'Alpha',
-        catCount: '1 cat'
-    }
-];
-
-const localeArrayItem = { name: 'Ärling', catCount: '' };
-
-describe('Sort by alphabetical order', () => {
-    beforeEach(() => collection = baseArray);
-    describe('Handle ascending order', () => {
-        beforeEach(() => {
-            collection = SortAlphabeticalOrder(collection, {
-                direction: SortDirections.Ascending,
-                sortKey: 'name'
-            });
-        });
-        test('Alpha should be first', () => expect(collection[0].name).toBe('Alpha'));
-        test('Zeta should be last', () => expect(collection[baseArray.length - 1].name).toBe('Zeta'));
-
-    });
-    describe('Handle descending order', () => {
-        beforeEach(() => {
-            collection = SortAlphabeticalOrder(collection, {
-                direction: SortDirections.Descending,
-                sortKey: 'name'
-            });
-        })
-
-        test('Zeta should be first', () => expect(collection[0].name).toBe('Zeta'));
-        test('Alpha should be last', () => expect(collection[collection.length - 1].name).toBe('Alpha'));
-    });
+describe('Sorting alphabetically', () => {
 
     describe('Handle diacritic characters', () => {
-        let indexOfZ: number, indexOfÄ: number;
-        // add an object with name that includes diacritic character
-        beforeEach(() => collection = [...baseArray, localeArrayItem]);
-
         describe('In German', () => {
             beforeEach(() => {
-                collection = SortAlphabeticalOrder(collection, {
+                returnVal = SortAlphabeticalOrder('ä', 'z', {
                     direction: SortDirections.Ascending,
-                    sortKey: 'name',
                     locale: 'de'
                 });
-                indexOfÄ = collection.findIndex(item => item.name === 'Ärling');
-                indexOfZ = collection.findIndex(item => item.name === 'Zeta');
             });
-            test('Ä sorts before z', () => expect(indexOfÄ).toBeLessThan(indexOfZ));
+            test('Ä sorts before z', () => expect(returnVal).toBeLessThan(0));
 
         });
         describe('In Swedish', () => {
             beforeEach(() => {
-                collection = SortAlphabeticalOrder(collection, {
+                returnVal = SortAlphabeticalOrder('ä', 'z', {
                     direction: SortDirections.Ascending,
-                    sortKey: 'name',
                     locale: 'se'
                 });
-                indexOfÄ = collection.findIndex(item => item.name === 'Ärling');
-                indexOfZ = collection.findIndex(item => item.name === 'Zeta');
             });
-            test('Ä sorts after z', () => expect(indexOfÄ).toBeGreaterThan(indexOfZ));
+            test('Ä sorts after z', () => expect(returnVal).toBeGreaterThan(0));
         });
     });
     describe('Handle numeric strings', () => {
 
-        beforeEach(() => collection = baseArray);
+        describe('Not sorting in ascending order', () => {
+            beforeEach(() => {
+                returnVal = SortAlphabeticalOrder("3 cats", "10 cats", {
+                    direction: SortDirections.Ascending,
+                    numeric: false
+                });
+
+                test('Second option should return first', () => expect(returnVal).toBeGreaterThan(0));
+
+            })
+        });
 
         describe('In ascending order', () => {
             beforeEach(() => {
-                collection = SortAlphabeticalOrder(collection, {
+                returnVal = SortAlphabeticalOrder("3 cats", "10 cats", {
                     direction: SortDirections.Ascending,
-                    sortKey: 'catCount',
                     numeric: true
                 });
             });
-            test('1 cat to be first', () => expect(collection[0].catCount).toBe('1 cat'));
-            test('10 cats to be last', () => expect(collection[collection.length - 1].catCount).toBe('10 cats'));
+            test('First option should return first', () => expect(returnVal).toBeLessThan(0));
         });
         describe('In descending order', () => {
             beforeEach(() => {
-                collection = SortAlphabeticalOrder(collection, {
+                returnVal = SortAlphabeticalOrder("1 cat", "3 cats", {
                     direction: SortDirections.Descending,
-                    sortKey: 'catCount',
                     numeric: true
                 });
             });
-            test('10 cats to be first', () => expect(collection[0].catCount).toBe('10 cats'));
-            test('1 cat to be last', () => expect(collection[collection.length - 1].catCount).toBe('1 cat'));
+            test('Second item should return first', () => expect(returnVal).toBeGreaterThan(0));
         });
-
     });
 }); 
--- src/sort/alphabetical/alphabetical.ts
+++ src/sort/alphabetical/alphabetical.ts
@@ -1,11 +1,22 @@
-import { SortDirections, SortOptions } from "../sort.interface";
+import { SortDirections, SortFunctionOptions } from "../sort.interface";
 
-export const SortAlphabeticalOrder = (collection: any[], options: SortOptions): any[] => {
-    return collection.sort((a: Record<string, string>, b: Record<string, string>) => {
-        return options.direction === SortDirections.Ascending
-            // sorting by ascending order
-            ? a[options.sortKey].localeCompare(b[options.sortKey], options.locale, options)
-            // sorting by descending order
-            : b[options.sortKey].localeCompare(a[options.sortKey], options.locale, options)
-    });
+
+export const SortAlphabeticalOrder = (first: string, second: string, options: SortFunctionOptions): number => {
+    // set fallback locale to english if missing.
+    const locale: string = options.locale || 'en';
+    return options.direction === SortDirections.Ascending
+        ? first.localeCompare(second, locale, options)
+        // sorting by descending order
+        : second.localeCompare(first, locale, options)
+
 }
+
+// export const SortAlphabeticalOrder = (collection: any[], options: SortOptions): any[] => {
+//     return collection.sort((a: Record<string, string>, b: Record<string, string>) => {
+//         return options.direction === SortDirections.Ascending
+//             // sorting by ascending order
+//             ? a[options.sortKey].localeCompare(b[options.sortKey], options.locale, options)
+//             // sorting by descending order
+//             : b[options.sortKey].localeCompare(a[options.sortKey], options.locale, options)
+//     });
+// }
