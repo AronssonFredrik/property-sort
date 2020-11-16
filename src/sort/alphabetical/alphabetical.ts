6fd61daec6d9cae8e5d42f6f4390e3aaf5afa5d4
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2020-11-16 19:55:35 +0100

    cleanup alphabetical ordering

--- src/sort/alphabetical/alphabetical.ts
+++ src/sort/alphabetical/alphabetical.ts
@@ -1,6 +1,5 @@
 import { SortDirections, SortFunctionOptions } from "../sort.interface";
 
-
 export const SortAlphabeticalOrder = (first: string, second: string, options: SortFunctionOptions): number => {
     // set fallback locale to english if missing.
     const locale: string = options.locale || 'en';
@@ -8,15 +7,4 @@ export const SortAlphabeticalOrder = (first: string, second: string, options: So
         ? first.localeCompare(second, locale, options)
         // sorting by descending order
         : second.localeCompare(first, locale, options)
-
 }
-
-// export const SortAlphabeticalOrder = (collection: any[], options: SortOptions): any[] => {
-//     return collection.sort((a: Record<string, string>, b: Record<string, string>) => {
-//         return options.direction === SortDirections.Ascending
-//             // sorting by ascending order
-//             ? a[options.sortKey].localeCompare(b[options.sortKey], options.locale, options)
-//             // sorting by descending order
-//             : b[options.sortKey].localeCompare(a[options.sortKey], options.locale, options)
-//     });
-// }
